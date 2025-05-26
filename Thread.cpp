72a5494179d76712b0d864ea4448da33ab0c4182
whuzhangkaiqi <zhangkaiqiovo@whu.edu.cn>
2025-05-26 13:34:06 +0800

    update: 线程类，基于C++特性实现

--- Thread.cpp
+++ Thread.cpp
@@ -1,1 +1,47 @@
 #include "Thread.hpp"
+#include "CurrentThread.hpp"
+#include <semaphore.h>
+
+std::atomic_int Thread::numCreated_ = 0;
+
+Thread::Thread(ThreadFunc func, const std::string& name)
+    :started_(false), joined_(false), tid_(0), func_(std::move(func)), name_(name) 
+{
+    setDefaultName();
+}
+Thread::~Thread()
+{
+    if (started_ && !joined_)
+    {
+        thread_->detach(); // thread类提供了设置分离线程的方法。在linux中本质上调用的还是pthread_detach;
+    }
+}
+void Thread::start()
+{
+    started_ = true;
+    sem_t sem;
+    sem_init(&sem, false, 0);
+    thread_ = std::shared_ptr<std::thread>(new std::thread([&](){
+        // 获取线程的tid值
+        tid_ = CurrentThread::tid();
+        sem_post(&sem);
+        func_(); 
+    }));
+    // 必须等待上面的新线程获取创建的tid值
+    sem_wait(&sem);
+}
+void Thread::join()
+{
+    joined_ = true;
+    thread_->join();
+}
+void Thread::setDefaultName()
+{
+    int num = ++numCreated_;
+    if (name_.empty())
+    {
+        char buf[32] = {0};
+        snprintf(buf, sizeof buf, "Thread %d", num);
+        name_ = buf;
+    }
+}
--- Thread.hpp
+++ Thread.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
 #include "noncopyable.hpp"
+#include <thread>
+#include <functional>
+#include <memory>
+#include <sys/unistd.h>
+#include <string>
+#include <atomic>
 
 class Thread : noncopyable
 {
     public:
-        
-    protected:
+        using ThreadFunc = std::function<void()>;
+        explicit Thread(ThreadFunc func, const std::string & name = std::string());
+        ~Thread();
 
+        void start();
+        void join();
+
+        bool started() const { return started_; }
+        pid_t tid() const { return tid_; } // 这里返回的不是真正的线程号
+        const std::string& name() const { return name_; }
+        static int numCreated() { return numCreated_; }
+
+
+    protected:
     private:
+        void setDefaultName();
+
+        bool started_;
+        bool joined_;
+        std::shared_ptr<std::thread> thread_;
+        pid_t tid_;
+        ThreadFunc func_;
+        std::string name_;
+        static std::atomic_int numCreated_;
 };
